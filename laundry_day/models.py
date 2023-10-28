f81e9ef6f9254d7ef81bbb0764dcc35c94bf1f1b
Emiliano Chavez De La Torre <111912343+emilianochavezDLT@users.noreply.github.com>
2023-10-27 20:27:55 -0600

    Added a User model

--- laundry_day/models.py
+++ laundry_day/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class User(models.Model):
+
+    # We just want to define the name of the user
+    # This just temporarily defines the user
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        db_table = 'custom_user'
+        
+    def get_absolute_url(self):
+        return reverse('user_detail', args=[str(self.id)])
+
+    parent = models.ForeignKey('self', 
+                               null=True, 
+                               blank=True, 
+                               related_name='children',
+                               on_delete=models.SET_NULL)    
+    
+    
+
+
+    
+        
+    
+    
+    
+    
+
+    
--- laundry_day/views.py
+++ laundry_day/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+from .models import *
 
 # Create your views here.
 def index(request):
     return render(request, 'laundry_day/index.html')
 
-
+class UserDetailView(generic.DetailView):
+    model = User
+    template_name = 'laundry_day/user_detail.html'
