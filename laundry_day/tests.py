d4f3bed5205444570547205131e9c99e5b606121
Emiliano Chavez De La Torre <111912343+emilianochavezDLT@users.noreply.github.com>
2023-11-17 15:01:58 -0700

    Comiit

--- laundry_day/tests.py
+++ laundry_day/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from laundry_day.models import User
 
 # Create your tests here.
+class ShowNameTest(TestCase):
+
+    #This is the setup method for getting a user
+    def setUp(self):
+        user = User.objects.get(id=3)
+        self.assertEqual(str(user.name), 'Alan Doe')
+
+
+
+
+    
